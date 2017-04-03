d092e3b307ef8ba1735b2b1985fc3ffafa31b9be
Don Knapp <don@dknapp.com>
2017-04-03 18:19:58 -0400

    Cleaned up the sequelize stuff.  The instructions call for a custom orm.

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const hbars   = require( "express-handlebars" );
 const methOverRide = require( "method-override" );
 const config  = require( "./config/config.json" );
 const mysql   = require( "mysql" );
-const sequelize = require( "./models" );
+const db = require( "./models" );
 const app = express();  // A new express application
 
 const port = 3000;
 
 console.log( config );
 
+db.sequelize.sync( { "force": true } );
 
-app.listen( config.port, () => {
+app.listen( port, () => {
     console.log( "Listening on port " + port );
 });
 
