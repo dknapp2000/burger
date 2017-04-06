fc4765eef9e36756b52651c819019104bdd5b04f
Don Knapp <don@dknapp.com>
2017-04-05 21:00:09 -0400

    Added auto reconnect to the database connection.

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,6 @@
 const mysql      = require( "mysql" );
 //const connection = require( "./connection.js" );
+let connected = false;
 
 const path = require( "path" );
 var env       = process.env.NODE_ENV || 'development';
@@ -20,12 +21,30 @@ db.connect( (err) => {
 		console.error("Error connecting to the DB: " + err );
 		return;
 	}
-
+	connected = true;
 	console.log( "Connected as id " + db.threadId );
 }) 
 
+db.on( "error", function( err ) {
+	console.log( "Recieved db error" , err );
+	connected = false;
+});
+
+function checkConnect() {
+	if ( connected ) return;
+	db.connect( (err ) => {
+		if (err) {
+			console.error("Error connecting to the DB: " + err );
+			return;
+		}
+		connected = true;
+		console.log( "reconnected as id " + db.threadId );
+	} )
+}
+
 function selectAll() {
 	return new Promise( function( resolve, reject ) {
+		checkConnect();
 		db.query( "SELECT * FROM burgers ORDER BY id", (err,result) => {
 			if ( err ) {
 				reject( err );
@@ -38,6 +57,7 @@ function selectAll() {
 
 function insertOne( burger ) {
 	return new Promise( function( resolve, reject ) {
+		checkConnect();
 		db.query( "INSERT INTO burgers ( burger_name, devoured ) values ( ?, ? )", [ burger.burger_name, burger.devoured ], (err,results) => {
 			if ( err ) {
 				reject( err );
@@ -51,6 +71,7 @@ function insertOne( burger ) {
 function updateOne( burger ) {
 	return new Promise( function( resolve, reject ) {
 		console.log( burger );
+		checkConnect();
 		db.query( "UPDATE burgers SET devoured = ? WHERE id = ?", [ burger.devoured, burger.id ], (err,results) => {
 			if ( err ) {
 				reject( err );
